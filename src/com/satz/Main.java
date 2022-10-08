24575fd5fcc924b699588638fe85a38a1d44fed9
SMylsamy <smylsamy@nextgen.com>
2022-10-08 18:58:49 +0530

    All Learn Java 1st git commit

---
+++ src/com/satz/Main.java
@@ -0,0 +1,17 @@
+package com.satz;
+
+
+
+
+public class Main {
+
+    static public void main(String[] args){
+    int num= 14;
+
+    /* HHHGHGG
+    JJJJHHH
+    KKK
+     */
+    System.out.println(num);
+    }
+}
---
+++ src/com/satz/stt.java
@@ -0,0 +1,4 @@
+package com.satz;
+
+public class stt {
+}
